5357e6f249cd1721d3a30b42a987654d818adc10
Duan-JM <vincent.duan95@bupt.edu.cn>
2019-11-04 16:34:13 +0800

    create whole architecture

--- conference_system/backend/module/base_process.py
+++
@@ -1,9 +0,0 @@
-"""
-File: process.py
-Author: Duan-JM
-Email: vincent.duan95@outlook.com
-Github: https://github.com/Duan-JM
-Description:
-    The module here is used the AI/none methods to process the data then give the actions' params
-"""
-
---
+++ conference_system/backend/module/process.py
@@ -0,0 +1,28 @@
+"""
+File: process.py
+Author: Duan-JM
+Email: vincent.duan95@outlook.com
+Github: https://github.com/Duan-JM
+Description:
+    The module here is used the AI/none methods to process the data
+    then give the actions' params
+"""
+class BasicProcess:
+    """
+    BaseClass for all module in ProcessModule
+    """
+    def set_device(self, data_source_device):
+        """
+        Point out which hardware to ouput
+
+        [optional]
+        """
+        pass
+
+    def take_action(self, params):
+        """
+        params: params: the params for the actions
+
+        return: flag(boolen): check if action have done properly
+        """
+        raise NotImplementedError
